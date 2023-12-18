881d7f008be8aed08cd9cef452591c067244c56d
Leo Tschritter <leotschritter@gmail.com>
2023-12-18 17:30:36 +0100

    kniffel_vue-main: Lobby Dialog with animation now and styled

--- src/components/lobbyComponents/LobbyDialogComponent.vue
+++ src/components/lobbyComponents/LobbyDialogComponent.vue
@@ -1,21 +1,17 @@
 <template>
   <q-dialog :model-value="dialogVisible" persistent @update:model-value="updateConfirmationDialog">
     <q-card>
-      <q-card-section>
-        <div class="container centering-block">
-          <div class="loader centered">
-            <!-- Your loader animation here -->
+      <q-card-section class="dialogBodyBox">
+          <lobby-loading-animation-component></lobby-loading-animation-component>
+          <div class="dialogBodyText">
+            <p>
+              <span>{{ playersReady }}</span>/<span>{{ playersCount }}</span>
+              Players ready
+            </p>
+            <p>
+              <span>Seconds until Start: </span><span>{{ countdown }}</span>
+            </p>
           </div>
-        </div>
-        <div>
-          <p>
-            <span>{{ playersReady }}</span>/<span>{{ playersCount }}</span>
-            Players ready
-          </p>
-          <p>
-            <span>Seconds until Start: </span><span>{{ countdown }}</span>
-          </p>
-        </div>
       </q-card-section>
 
       <q-card-actions align="center">
@@ -23,14 +19,7 @@
           flat
           color="dark"
           id="ready"
-          @click="readyFunction"
-          :style="{ backgroundColor: ready ? 'green' : '', borderColor: ready ? 'green' : '' }"
-        >
-          <span class="material-symbols-outlined">
-          <q-icon v-if="ready">check</q-icon>
-          </span>
-          Ready
-        </q-btn>
+          @click="readyFunction">Ready</q-btn>
         <q-btn flat color="dark" id="leaveLobby" @click="openLeaveDialog">
           Leave
           <span class="material-symbols-outlined">
@@ -43,7 +32,10 @@
 </template>
 
 <script>
+import LobbyLoadingAnimationComponent from "components/lobbyComponents/LobbyLoadingAnimationComponent.vue";
+
 export default {
+  components: {LobbyLoadingAnimationComponent},
   props: {
     dialogVisible: Boolean,
     playersReady: Number,
@@ -53,7 +45,17 @@ export default {
   },
   methods: {
     readyFunction() {
-      this.$emit('readyFunction');
+      const readyBtn = document.getElementById('ready')
+      readyBtn.style.backgroundColor = 'green'
+      readyBtn.style.borderColor = 'green'
+      readyBtn.style.display = 'flex'
+      readyBtn.style.justifyContent = 'center'
+      readyBtn.disabled = true
+      const checkSpan = document.createElement('span')
+      checkSpan.classList.add('material-symbols-outlined')
+      checkSpan.innerText = 'check'
+      readyBtn.replaceChildren(checkSpan)
+      this.$emit('readyFunction')
     },
     openLeaveDialog() {
       this.$emit('openLeaveDialog');
@@ -69,9 +71,29 @@ export default {
 
 .q-card {
   background: #222;
+  overflow: hidden;
 }
 
 .q-btn {
   background: darkgrey;
+}
+
+.dialogBodyBox {
+  display: flex;
+}
+
+.dialogBodyText {
+  text-align: center;
+  color: white;
+}
+
+#ready {
+  width: 100px;
+  height: 20px;
+}
+
+#leaveLobby {
+  width: 100px;
+  height: 20px;
 }
 </style>
