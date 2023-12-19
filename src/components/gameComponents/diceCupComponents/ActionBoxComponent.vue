4a5e31ed5a6abe4802d76f4aae0a757b05b6963c
Leo Tschritter <leotschritter@gmail.com>
2023-12-19 13:58:01 +0100

    kniffel_vue-main: RemainingDiceImageComponent created, tested and used now

--- src/components/gameComponents/diceCupComponents/ActionBoxComponent.vue
+++ src/components/gameComponents/diceCupComponents/ActionBoxComponent.vue
@@ -1,8 +1,10 @@
 <script>
-import DiceCupActionButtonComponent from 'components/gameComponents/diceCupComponents/DiceCupActionButtonComponent.vue';
+import DiceCupActionButtonComponent from 'components/gameComponents/diceCupComponents/DiceCupActionButtonComponent.vue'
+import RemainingDiceImageComponent from "components/gameComponents/diceCupComponents/RemainingDiceImageComponent.vue"
 export default {
   components: {
-    DiceCupActionButtonComponent
+    DiceCupActionButtonComponent,
+    RemainingDiceImageComponent
   },
   props: {
     remainingDices: Number,
@@ -21,17 +23,9 @@ export default {
 
 <template>
   <div class="action-box">
-    <q-img src="src/assets/images/dicecup_small.png" id="remDice3" :style="{
-      opacity: remainingDices < 2 ? 0.3 : 1
-    }"/>
-
-    <q-img src="src/assets/images/dicecup_small.png" id="remDice2" :style="{
-      opacity: remainingDices < 1 ? 0.3 : 1
-    }"/>
-
-    <q-img src="src/assets/images/dicecup_small.png" id="remDice1" :style="{
-      opacity: remainingDices < 0 ? 0.3 : 1
-    }"/>
+    <remaining-dice-image-component id="remDice3" :opacity="remainingDices < 2 ? 0.3 : 1"></remaining-dice-image-component>
+    <remaining-dice-image-component id="remDice2" :opacity="remainingDices < 1 ? 0.3 : 1"></remaining-dice-image-component>
+    <remaining-dice-image-component id="remDice1" :opacity="remainingDices < 0 ? 0.3 : 1"></remaining-dice-image-component>
     <dice-cup-action-button-component @putAllIn="putAllIn" :disabled="!active || remainingDices < 0" id="allInButton" :is-dice-button="false">
     </dice-cup-action-button-component>
     <dice-cup-action-button-component @dice="dice" :disabled="remainingDices < 0 || !active" :is-dice-button="true">
---
+++ src/components/gameComponents/diceCupComponents/RemainingDiceImageComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-img src="src/assets/images/dicecup_small.png" :id="this.id" :style="{ opacity: this.opacity }"/>
+</template>
+
+<script>
+export default {
+  props: {
+    id: String,
+    opacity: Number
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
