7129143d900f6db501e34f51f6a43d2097144d63
Leo Tschritter <leotschritter@gmail.com>
2024-01-12 17:46:38 +0100

    WAPP-10: Fixed DiceCup sizes

--- src/components/gameComponents/diceCupComponents/InCupComponent.vue
+++ src/components/gameComponents/diceCupComponents/InCupComponent.vue
@@ -33,65 +33,48 @@ export default {
 </template>
 
 <style scoped>
-.cup {
-  width: 227px;
-  height: 229px;
-  //scale: 75%;
-}
-@media screen and (max-width: 600px) {
-  .cup {
-    width: 165px;
-    height: 166px;
-  }
-  .cup.showCup {
-    background-size: 165px !important;
-    background-repeat: no-repeat !important;
-  }
-  #diceInCup {
-    left: 0;
-  }
-}
-
-
-@media screen and (max-width: 450px) {
-  .cup {
-    margin-right: 10px;
-    width: 165px;
-    height: 166px;
-  }
-  .cup.showCup {
-    background-size: 165px !important;
-    background-repeat: no-repeat !important;
-  }
-  #diceInCup {
-    left: 0;
-  }
-}
-
-@media screen and (max-width: 350px) {
-  .cup {
-    width: 175px;
-    height: 100px;
-  }
-  .cup.showCup {
-    background-size: 165px !important;
-    background-repeat: no-repeat !important;
-  }
-
-  #diceInCup {
-    left: 0;
-  }
-}
-
-
 #diceInCup {
   padding: 52px 65px;
   width: 290px;
   display: inline-block;
   position: relative;
-  left: -32px;
+  left: -45px;
   top: 0px;
   visibility: hidden;
+}
+
+.cup {
+  width: 190px;
+  height: 190px;
+  background-size: 190px;
+}
+@media screen and (max-width: 600px) {
+  .cup {
+    width: 165px;
+    height: 165px;
+  }
+  .cup.showCup {
+    background-size: 165px;
+    background-repeat: no-repeat;
+  }
+  #diceInCup {
+    left: -55px;
+    top: -15px;
+  }
+}
+
+@media screen and (max-width: 350px) {
+  .cup.showCup {
+    height: 140px;
+    width: 140px;
+    background-size: 140px;
+    background-repeat: no-repeat !important;
+  }
+
+  #diceInCup {
+    left: -65px;
+    top: -25px;
+  }
 }
 
 @keyframes shake {
@@ -107,11 +90,6 @@ export default {
   40%, 60% {
     transform: translate3d(16px, 16px, 0);
   }
-}
-
-.cup {
-  width: 190px;
-  height: 229px;
 }
 
 </style>
